1e859d967fcc1fca41358fd7fbc2a2fbe4cf63e7
Jack Honour <gangnam253@gmail.com>
2018-06-14 13:35:20 +0100

    Update main.php

--- src/Jack/LuckyBlock/main.php
+++ src/Jack/LuckyBlock/main.php
@@ -10,15 +10,14 @@ use pocketmine\utils\Config;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
-use pocketmine\event\player\{PlayerJoinEvent,PlayerQuitEvent, PlayerDeathEvent, PlayerChatEvent};;
 
 class main extends PluginBase implements Listener{
 		
 	public function onEnable(){
         if (!is_dir($this->getDataFolder())) {
             @mkdir($this->getDataFolder());
-            //Use default, not PM.
         }
+		
 	}
 	
 	public function onDisable(){
