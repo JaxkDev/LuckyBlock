b4b66f9435fbb394b0b0570eec0ce9e3315f0a3c
Jack Honour <gangnam253@gmail.com>
2018-06-18 13:45:56 +0100

    Update
    Needs testing.

--- src/Jack/LuckyBlock/events.php
+++ src/Jack/LuckyBlock/events.php
@@ -3,10 +3,7 @@ namespace Jack\LuckyBlock;
 
 use pocketmine\Player;
 use pocketmine\Server;
-<<<<<<< HEAD
 use pocketmine\entity\Entity;
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
 use pocketmine\item\Item;
 use pocketmine\block\Block;
 use pocketmine\utils\Config;
@@ -14,8 +11,6 @@ use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as C;
-<<<<<<< HEAD
-
 use pocketmine\nbt\NBT;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\DoubleTag;
@@ -23,8 +18,6 @@ use pocketmine\nbt\tag\FloatTag;
 use pocketmine\nbt\tag\IntTag;
 use pocketmine\nbt\tag\ListTag;
 use pocketmine\nbt\tag\StringTag;
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
 use pocketmine\event\block\{BlockBreakEvent};;
 class events implements Listener
 {
@@ -33,7 +26,6 @@ class events implements Listener
     {
         $this->main = $plugin;
     }
-<<<<<<< HEAD
 
     public function isExistsEntity($name): bool
     {
@@ -49,14 +41,12 @@ class events implements Listener
             ])
         ]);
         $name = str_replace(" ", "", ucwords($name));
-        $entity = Entity::createEntity($name, $this->main->getServer()->getDefaultLevel()->getChunk(0, 0, true), $nbt);
+        $entity = Entity::createEntity($name, $this->main->getServer()->getDefaultLevel(), $nbt);
         if (!($entity instanceof Entity))
             return false;
         $entity->close();
         return true;
     }
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
     
     public function blockBreak(BlockBreakEvent $event)
     {
@@ -73,32 +63,23 @@ class events implements Listener
             }
             $player = $event->getPlayer();
             $event->setCancelled();
-            $rand = rand(1, 10);
-<<<<<<< HEAD
+            $rand = rand(1, 5);
             if($this->main->cfg->get('debug') == true){
                 $this->main->getLogger()->info('Got number: '.$rand);
             }
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
             switch($rand){
                 case 1:
-                case 2:
-                case 3:
                     //Give them some $$$
-<<<<<<< HEAD
                     $player->getLevel()->setBlock($block, new Block(Block::AIR), true, true);
-                    if(isset($this->main->economy) == false) break;
+                    if(!isset($this->main->economy)){
+                        break;
+                    }
                     $mon = rand($this->main->cfg->get('rewards')['money']['min'], $this->main->cfg->get('rewards')['money']['max']);
                     $this->main->economy::getInstance()->addMoney($player, $mon);
                     $player->sendMessage(C::GOLD."Hmmm,  £".$mon."Magically transferred into your bank balance !");
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
                     break;
-
-                case 4:
-                case 5:
+                case 2:
                     //Give em a mob
-<<<<<<< HEAD
                     $player->getLevel()->setBlock($block, new Block(Block::AIR), true, true);
                     $nbt = new CompoundTag("", [
                         new ListTag("Pos", [
@@ -115,39 +96,23 @@ class events implements Listener
                     $entity = Entity::createEntity('Cow', $player->getLevel(), $nbt);
                     $entity->spawnToAll();
                     $player->sendMessage("Woah look at what appeared !");
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
                     break;
-
-                case 6:
-                case 7:
+                case 3:
                     //What about a chest...
-<<<<<<< HEAD
                     $player->getLevel()->setBlock($block, new Block(Block::CHEST), true, true);
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
                     break;
-
-                case 8:
-                case 9:
+                case 4:
                     //Do absolute nothing, RIP
-<<<<<<< HEAD
+                    $player->getLevel()->setBlock($block, new Block(Block::AIR), true, true);
                     $player->sendMessage('Ooops must have forgot to do something for you, Oh Well !');
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
                     break;
-                
-                case 10:
+                case 5:
                     //change block to dirt, what good exchange rate.
-<<<<<<< HEAD
-                    $player->getLevel()->setBlock($block, new Block(Block::DIRT), true, true);
-=======
->>>>>>> 7b69acd7c154879de87b3c6754263a03896e7f71
+                    $player->getLevel()->setBlock($block, new Block(Block::GRASS), true, true);
                     break;
                     
             }
-            $player->sendPopup($rand);
-            //right block so lets do stuff
+            $player->sendPopup($rand); //for debug
         }
     }
 }
